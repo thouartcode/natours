7d8d73e9d8972513b9cc704cd014126746591251
thouartcode <anugrah.pateria@gmail.com>
2021-01-29 15:24:15 +0530

    implemented a better error handeling method and dev and prod errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    messsage: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operation, trusted error: send message to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      messsage: err.message
+    });
+    // Programming or other unknown error: don't leak error details
+  } else {
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    messsage: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
