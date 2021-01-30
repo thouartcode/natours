e82a7744f5e181b137c237be66e4aaf96c9dd697
thouartcode <anugrah.pateria@gmail.com>
2021-01-30 11:25:16 +0530

    implemented unhandled rejection and uncaught errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,22 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = err => {
+  const value = err.errmsg.match(/(["'])(?:\\.|[^\\])*?\1/)[0];
+  const message = `Duplicate field value ${value}. Try different value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data! ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -16,6 +35,10 @@ const sendErrorProd = (err, res) => {
     });
     // Programming or other unknown error: don't leak error details
   } else {
+    // 1) Log the error
+    console.error('ERROR:', err);
+    // 2) send generic message
+
     res.status(500).json({
       status: 'Error',
       message: 'Something went very wrong!'
@@ -30,6 +53,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
