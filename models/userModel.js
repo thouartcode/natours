61f341fcff7f0d4845ac712c9ad6da16a675df04
thouartcode <anugrah.pateria@gmail.com>
2021-01-31 15:43:50 +0530

    implemented user models and manage passwords

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!.']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email!']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'A password id required'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!!'
+    }
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
