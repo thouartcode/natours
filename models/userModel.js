1812a87e6d474d7453a7882903a1bf52bbf71fe6
thouartcode <anugrah.pateria@gmail.com>
2021-01-31 15:54:24 +0530

    implemented password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,10 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
