085d2fca09df0a5c122e244454ec795840f2331c
thouartcode <anugrah.pateria@gmail.com>
2021-02-02 14:32:35 +0530

    Implemented route protection

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,9 +71,14 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
 
   // 3) Check if user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError('The user with this token no longer exists.', 401)
+    );
+  }
 
   // 4) Check if user changed the password after the token was issued
   next();
