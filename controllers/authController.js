7ff43de96d30e4dbd88f94568487d8cbf2f972c7
thouartcode <anugrah.pateria@gmail.com>
2021-02-04 15:54:50 +0530

    implemented authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,3 +92,18 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = freshUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide']. role = user
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError(
+          `You don't have the permission to perform this action!`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
