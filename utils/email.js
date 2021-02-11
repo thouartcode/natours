e257f608cebd9681a7907339b77eeea6afc0b3f4
thouartcode <anugrah.pateria@gmail.com>
2021-02-11 17:16:35 +0530

    implemented email rest token

---
+++ utils/email.js
@@ -0,0 +1,26 @@
+const nodeMailer = require('nodemailer');
+
+const sendEmail = async options => {
+  // 1) Create the transporter
+  const transporter = nodeMailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Anugrah Pateriya <anugrah@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  // 3) Send the actual email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
